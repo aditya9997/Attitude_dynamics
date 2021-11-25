b41449cc1148fc739775efbc81da25c690f7816b
BIGDUST0 <davide.finco99@gmail.com>
2021-11-25 22:36:21 +0100

    modifica davide

--- Lab7 Giovanni/Lab7.m
+++ Lab7 Giovanni/Lab7.m
@@ -30,12 +30,12 @@ n = sqrt(muP/a^3);
 RS_MB = 0.5;
 RS_SP = 0.1;
 RD_MB = 0.1*ones(6,1);
-RD_SP = 0.1*ones(4,1);
+RD_SP = 1*ones(4,1);
 A_MB = 0.5; %[m^2]
 A_SP = 1; %[m^2]
 c = 299792*10^3; %[m/s]
 n_Sun = 2*pi/(365*24*60*60);
-eps = degtorad(23.45);
+eps = deg2rad(23.45);
 w_E = (2*pi)/(24*3600); %angular veloity of earth
 
 %Defining terms for computing H0 of magnetic field (see slides of Lab 7).
