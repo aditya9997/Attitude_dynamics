fa1a0b8ac2cfe488bb6cc048300aaa83dbd8a9f8
BIGDUST0 <davide.finco99@gmail.com>
2021-12-19 11:01:35 +0100

    ok

--- Laboratorio 7 Davide/Lab 7/Main/Main.m
+++ Laboratorio 7 Davide/Lab 7/Main/Main.m
@@ -1,6 +1,6 @@
-clear all;
-close all;
-clc;
+clear all
+close all
+clc
 
 %% definisco dati
 
@@ -12,6 +12,7 @@ a = 8.6251e+06;
 e = 0.1195;
 i = 0.3815;
 n = sqrt(muP/a^3);  
+T = 2*pi/n;
 
 %SRP
 RS_MB = 0.5;
@@ -28,7 +29,7 @@ RD_SP = 0.1*ones(4,1);
 A_SP = 1; %[m^2]
 c = 299792*10^3; %[m/s]
 n_Sun = 2*pi/(365*24*60*60);
-eps = degtorad(23.45);
+eps = deg2rad(23.45);
 w_E = (2*pi)/(24*3600); %angular veloity of earth
 
 %Defining terms for computing H0 of magnetic field (see slides of Lab 7).
@@ -55,4 +56,25 @@ SP = [2000; 0.3; 1; 0.12].*1e-2;
 %magnetometer
 p_acc = 0.5; %[degree], look at slide 12 of attitude sensors, bernelli's slide
 f_mag = 18; %[Hz] by https://www.cubesatshop.com/product/nss-magnetometer/, in alternative we can put f=5 since bernelli's slides
-% 
+
+% Reaction Wheels (using 4 RW with the 3 axis + diagonal model)
+A_rw = [1 0 0 1/sqrt(3);...
+        0 1 0 1/sqrt(3);
+        0 0 1 1/sqrt(3)];
+
+A_rw_star =  [5/6 -1/6 -1/6;...
+             -1/6  5/6 -1/6;
+             -1/6 -1/6  5/6;
+              1/(2*sqrt(3)) 1/(2*sqrt(3)) 1/(2*sqrt(3))];
+
+
+% %Gyro
+t_sample = 1/5;
+sig_b = 0.3*pi/180/3600/sqrt(t_sample);
+sig_n = 0.15*pi/180/3600/sqrt(t_sample);
+sig_b = sig_b^2;
+
+mr = 1.2*1e-4;
+p_acc = deg2rad(5);
+f_mag = 5;
+
