e3171987c71b310efdbf25113b3173f0fd061e17
BIGDUST0 <davide.finco99@gmail.com>
2021-12-24 17:15:43 +0100

    todo ok

--- Laboratorio 7 Davide/Lab 7/Main/Main.m
+++ Laboratorio 7 Davide/Lab 7/Main/Main.m
@@ -14,6 +14,11 @@ i = 0.3815;
 n = sqrt(muP/a^3);  
 T = 2*pi/n;
 
+%initial condition A_BN
+A_BN_0 =  [1/3, 2/3, 2/3; ...
+           2/3, 1/3, -2/3; ...
+          -2/3, 2/3,-1/3];
+
 %SRP
 RS_MB = 0.5;
 RS_SP = 0.1;
@@ -29,7 +34,7 @@ RD_SP = 0.1*ones(4,1);
 A_SP = 1; %[m^2]
 c = 299792*10^3; %[m/s]
 n_Sun = 2*pi/(365*24*60*60);
-eps = degtorad(23.45);
+eps = deg2rad(23.45);
 w_E = (2*pi)/(24*3600); %angular veloity of earth
 
 %Defining terms for computing H0 of magnetic field (see slides of Lab 7).
@@ -77,3 +82,7 @@ sig_b = sig_b^2;
 mr = 1.2*1e-4;
 p_acc = deg2rad(5);
 f_mag = 5;
+
+% Control of Slew manoeuvre
+k1 = 1;
+k2 = 3;
